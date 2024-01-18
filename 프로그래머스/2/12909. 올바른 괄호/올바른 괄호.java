b04ae123628f50222187fd12e09378765e2e5e43
ksb4150 <74931459+ksb4150@users.noreply.github.com>
2024-01-18 13:27:35 +0900

    [level 2] Title: 올바른 괄호, Time: 9.93 ms, Memory: 54.1 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,17 +1,29 @@
 class Solution {
     boolean solution(String s) {
-        boolean answer = true;
-
-        char[] arr = s.toCharArray();
+        int len = s.length();
         
+        if(len % 2 != 0){ 
+            return false;
+        }
+        if(s.substring(0,1).equals(")") || s.substring(len-1).equals("(")) { 
+            return false;
+        }
+        if(len - s.replace(")","").length() != len/2){
+            return false;
+        }
+        if(len - s.replace("(","").length() != len/2){
+            return false;
+        }
+                
         int flag = 0;
-        for(int i =0; i<arr.length; i++) {
-            flag += arr[i] == '(' ? 1 : -1;
-            if(flag == -1) {
-            	break;
+        for(int i=0; i<len; i++){
+            flag += s.charAt(i) == ')' ? -1 : 1;
+            if(flag < 0){
+                return false;
             }
         }
-
-        return flag != 0 ? false : true;
+        
+        
+        return true;
     }
 }
