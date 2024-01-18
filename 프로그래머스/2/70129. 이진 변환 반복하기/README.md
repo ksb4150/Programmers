c87ff4af0e45e2a76be81f567be6c13c82ced047
ksb4150 <74931459+ksb4150@users.noreply.github.com>
2024-01-18 16:34:37 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 54.19 ms, Memory: 108 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/README.md
+++ 프로그래머스/2/70129. 이진 변환 반복하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 이진 변환 반복하기 - 70129 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/70129?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/70129) 
 
 ### 성능 요약
 
-메모리: 82.3 MB, 시간: 13.92 ms
+메모리: 108 MB, 시간: 54.19 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 2일 0:22:32
+2024년 1월 4일 16:34:34
 
 ### 문제 설명
 
--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
@@ -1,16 +1,23 @@
-import java.util.*;
-
 class Solution {
     public int[] solution(String s) {
-        int[] answer = new int[2];
-
-        while(s.length() != 1) {
-            answer[1] += s.length() - s.replace("0", "").length();
-            s = s.replace("0", "");
-            int binary = s.length();
-            s = Integer.toBinaryString(binary);
+        int[] answer = { 0,0 };
+        int zeroCnt = 0;
+        int changeCnt = 0;
+        
+        while(s.length() > 1){
             answer[0]++;
+            int cnt = s.replace("0", "").length();
+            answer[1] += s.length() - s.replaceAll("0", "").length();
+            String str = s.replaceAll("0", "");
+            int len = str.length();
+            if(len == 1){
+                break;                
+            }
+            String binaryString = Integer.toBinaryString(len);
+            s = binaryString;
         }
+        
+        
         return answer;
     }
 }
