d48f5eff8fae281bbe4b4feaf07e8dd279b83f53
ksb4150 <74931459+ksb4150@users.noreply.github.com>
2025-01-15 21:30:10 +0900

    [level 0] Title: 꼬리 문자열, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.py
+++ 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.py
@@ -1,8 +1,8 @@
 def solution(str_list, ex):
     answer = ''
-    for str in str_list:
-        if str.find(ex) > -1:
+    for i in str_list:
+        if i.find(ex) > -1:
             answer += ''
         else:
-            answer += str
+            answer += i
     return answer
