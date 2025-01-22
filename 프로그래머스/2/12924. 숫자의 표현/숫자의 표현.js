4c001bf0c6d484a7f91f2f98aa133cea27bd188c
ksb4150 <74931459+ksb4150@users.noreply.github.com>
2025-01-22 21:33:25 +0900

    [level 2] Title: 숫자의 표현, Time: 0.30 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
@@ -1,24 +1,10 @@
-function solution(n) {    
-    const answer = [];
-    let numbers = [];
-    let i = 1,
-        j = 1;
-
-    while(i <= n){
-        if(i > n) break;        
-        numbers.push(i);
-        let total = 0;
-        for(const num of numbers) 
-            total += num;
-        if(total === n){ 
-            answer.push(numbers.shift());                 
-            i = j++;             
-        }else if(total > n){
-            numbers = [];
-            i = j++;
-        }        
-        i++;
+function solution(num) {
+    var answer = 0;
+    for (var i = 1; i <= num; i++) {
+        if ((num % i == 0) && (i % 2 == 1)) {
+            answer++;
+        }
     }
+    return answer;
 
-    return answer.length;
 }
